391ecf82b3b84626975fc9804b78aed4c9faccc9
EinfachMichi <94147049+EinfachMichi@users.noreply.github.com>
2024-01-17 21:49:59 +0100

    Backup

--- Source/PuzzleGame/BuildingManager.cpp
+++ Source/PuzzleGame/BuildingManager.cpp
@@ -19,47 +19,92 @@ void UBuildingManager::BeginPlay()
 			CropSpots.Add(CropSpot);
 		}
 	}
+
+	CameraComponent = GetOwner()->FindComponentByClass<UCameraComponent>();
 }
 
 void UBuildingManager::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	ShootLineTrace();
+	PreviewPlaceableActor();
 }
 
-FHitResult UBuildingManager::GetLineTraceHitResult(FVector WorldLocation, FVector ForwardVector, float Distance)
+FHitResult UBuildingManager::GetLineTraceHitResult()
+{
+	return LineTraceHitResult;
+}
+
+void UBuildingManager::SetActorToPlace(AActor* Actor)
+{
+	this->ActorToPlaceTemplate = Actor;
+}
+
+void UBuildingManager::PlaceActor()
+{
+	if(ActorToPlace)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Actor successfully placed"));
+		UE_LOG(LogTemp, Error, TEXT("Weird: %s"), *ActorToPlace->GetName());
+		ActorToPlace = nullptr;
+	}
+}
+
+void UBuildingManager::PreviewPlaceableActor()
+{
+	if(!ActorToPlaceTemplate || !CanPlaceMaterial || !CanNotPlaceMaterial)
+	{
+		return;
+	}
+
+	if(!ActorToPlace)
+	{
+		UClass* ActorClass = ActorToPlaceTemplate->GetClass();
+		FVector ActorLocation = ActorToPlaceTemplate->GetActorLocation();
+		FRotator ActorRotation = ActorToPlaceTemplate->GetActorRotation();
+		FActorSpawnParameters SpawnParameters;
+		ActorToPlace = GetWorld()->SpawnActor<AActor>(ActorClass, ActorLocation, ActorRotation, SpawnParameters);
+	}
+
+	
+}
+
+void UBuildingManager::ShootLineTrace()
 {
 	FHitResult HitResult;
 	
 	AActor* Owner = GetOwner();
-	if (!Owner)
+	if (!Owner || !CameraComponent)
 	{
-		return HitResult;
+		return;
 	}
 	
-	FVector EndTrace = WorldLocation + ForwardVector * Distance;
+	FVector Start = CameraComponent->GetComponentLocation();
+	FVector End = Start + CameraComponent->GetForwardVector() * LineTraceDistance;
 	FCollisionQueryParams CollisionParams;
 	CollisionParams.AddIgnoredActor(Owner);
 	
 	bool Success = GetWorld()->LineTraceSingleByChannel(
 		HitResult,
-		WorldLocation,
-		EndTrace,
-		ECC_PhysicsBody,
+		Start,
+		End,
+		ECC_Visibility,
 		CollisionParams
 	);
 
+	LineTraceHitResult = HitResult;
+	
 	if(Debug)
 	{
 		if (Success)
 		{
-			DrawDebugLine(GetWorld(), WorldLocation, HitResult.ImpactPoint, FColor::Green, false, 0, 0, 2);
+			DrawDebugLine(GetWorld(), Start, HitResult.ImpactPoint, FColor::Green, false, 0, 0, 2);
 			DrawDebugPoint(GetWorld(), HitResult.ImpactPoint, 10, FColor::Red, false, 0);
 		}
 		else
 		{
-			DrawDebugLine(GetWorld(), WorldLocation, EndTrace, FColor::Red, false, 0, 0, 2);
+			DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 0, 0, 2);
 		}
 	}
-	
-	return HitResult;
 }
--- Source/PuzzleGame/BuildingManager.h
+++ Source/PuzzleGame/BuildingManager.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "CropSpot.h"
+#include "Camera/CameraComponent.h"
 #include "Components/ActorComponent.h"
 #include "BuildingManager.generated.h"
 
@@ -11,20 +12,46 @@ class PUZZLEGAME_API UBuildingManager : public UActorComponent
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(EditAnywhere, Category="Utils")
+	float LineTraceDistance;
+
+	UPROPERTY(BlueprintReadOnly, Category="Utils")
+	FHitResult LineTraceHitResult;
+	
+private:
 	UPROPERTY(EditAnywhere, Category="Debug")
 	bool Debug = false;
 
+	UPROPERTY(EditAnywhere, Category="Build System")
+	UMaterialInstance* CanPlaceMaterial;
+	
+	UPROPERTY(EditAnywhere, Category="Build System")
+	UMaterialInstance* CanNotPlaceMaterial;
+
+	AActor* ActorToPlaceTemplate;
+	AActor* ActorToPlace;
+	UCameraComponent* CameraComponent;
 	TArray<ACropSpot*> CropSpots;
 
 public:	
 	UBuildingManager();
 
-protected:
-	virtual void BeginPlay() override;
-
-public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
 	UFUNCTION(BlueprintCallable, Category="Utils")
-	FHitResult GetLineTraceHitResult(FVector WorldLocation, FVector ForwardVector, float Distance);
+	FHitResult GetLineTraceHitResult();
+	
+	UFUNCTION(BlueprintCallable, Category="Build System")
+	void SetActorToPlace(AActor* Actor);
+
+	UFUNCTION(BlueprintCallable, Category="Build System")
+	void PlaceActor();
+	
+private:
+	void PreviewPlaceableActor();
+	void ShootLineTrace();
+
+protected:
+	virtual void BeginPlay() override;
 };
