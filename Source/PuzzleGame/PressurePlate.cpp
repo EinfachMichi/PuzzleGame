4b0e7b9a866ed8bf453631fffa784f7de4550b1e
EinfachMichi <94147049+EinfachMichi@users.noreply.github.com>
2024-01-20 17:29:47 +0100

    Map rework + pressure plate display

--- Source/PuzzleGame/PressurePlate.cpp
+++ Source/PuzzleGame/PressurePlate.cpp
@@ -1,5 +1,4 @@
 #include "PressurePlate.h"
-
 #include "Crop.h"
 
 APressurePlate::APressurePlate()
@@ -9,6 +8,21 @@ APressurePlate::APressurePlate()
 	DefaultSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneComponent"));
 	DefaultSceneComponent->SetupAttachment(RootComponent);
 
+	SignSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Sign"));
+	SignSceneComponent->SetupAttachment(DefaultSceneComponent);
+	
+	DisplaySceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Display"));
+	DisplaySceneComponent->SetupAttachment(SignSceneComponent);
+	
+	FirstMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("First"));
+	FirstMesh->SetupAttachment(DisplaySceneComponent);
+	
+	SecondMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Second"));
+	SecondMesh->SetupAttachment(DisplaySceneComponent);
+	
+	ThirdMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Third"));
+	ThirdMesh->SetupAttachment(DisplaySceneComponent);
+	
 	PressurePlateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PressurePlateStaticMeshComponent"));
 	PressurePlateMesh->SetupAttachment(DefaultSceneComponent);
 	
@@ -35,4 +49,38 @@ float APressurePlate::GetMass()
 		}
 	}
 	return Mass;
+}
+
+void APressurePlate::UpdateDisplay()
+{
+	int Mass = GetMass();
+	
+	for (int i = 0; i < 3; i++)
+	{
+		if(Mass < 0)
+		{
+			return;
+		}
+		
+		int Digit = Mass % 10;
+		Mass /= 10;
+		
+		ChangeMesh(i, Digit);
+	}
+}
+
+void APressurePlate::ChangeMesh(int Mesh, int Number)
+{
+	switch (Mesh)
+	{
+	case 0:
+		ThirdMesh->SetStaticMesh(NumberMeshes[Number]);
+		break;
+	case 1:
+		SecondMesh->SetStaticMesh(NumberMeshes[Number]);
+		break;
+	case 2:
+		FirstMesh->SetStaticMesh(NumberMeshes[Number]);
+		break;
+	}
 }
--- Source/PuzzleGame/PressurePlate.h
+++ Source/PuzzleGame/PressurePlate.h
@@ -13,16 +13,40 @@ class PUZZLEGAME_API APressurePlate : public AActor
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
 	USceneComponent* DefaultSceneComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	USceneComponent* SignSceneComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	USceneComponent* DisplaySceneComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
 	UStaticMeshComponent* PressurePlateMesh;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
 	UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere, Category="Display")
+	TArray<UStaticMesh*> NumberMeshes;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	UStaticMeshComponent* FirstMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	UStaticMeshComponent* SecondMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	UStaticMeshComponent* ThirdMesh;
 	
 public:	
 	APressurePlate();
 
 	UFUNCTION(BlueprintCallable, Category="Pressure Plate")
 	float GetMass();
+
+	UFUNCTION(BlueprintCallable, Category="Pressure Plate")
+	void UpdateDisplay();
+
+private:
+	void ChangeMesh(int Mesh, int Number);
 };
